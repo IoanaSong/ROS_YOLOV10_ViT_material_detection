4679310203a14bb518d3eec2e462b57313b59c78
Ioana Song <62600436+IoanaSong@users.noreply.github.com>
2024-07-17 01:17:37 +0200

    Material detection workingg and outputting class

--- scripts/yolo_vit_mat_detector.py
+++ scripts/yolo_vit_mat_detector.py
@@ -25,10 +25,17 @@ from vit_inference.msg import MaterialDetected
 processor = AutoImageProcessor.from_pretrained("ioanasong/vit-MINC-2500")
 model = AutoModelForImageClassification.from_pretrained("ioanasong/vit-MINC-2500") 
 
+# labels_materials = ['brick', 'carpet','ceramic','fabric','foliage', 'food','glass','hair',
+#                     'leather','metal','mirror','other','painted','paper','plastic','polishedstone',
+#                     'skin','sky','stone','tile','wallpaper','water','wood']
+
 
 class YoloVitMaterialDetector:
     def __init__(self):
         self.bridge = CvBridge()
+        self.labels_materials = ['brick', 'carpet', 'ceramic', 'fabric', 'foliage', 'food', 'glass', 'hair',
+                             'leather', 'metal', 'mirror', 'other', 'painted', 'paper', 'plastic', 'polishedstone',
+                             'skin', 'sky', 'stone', 'tile', 'wallpaper', 'water', 'wood']
         # self.yolo_model = YOLO('yolov10n.pt')  # "Suitable for extremely resource-constrained environments" for object-detection
         self.yolo_model = YOLO('yolov10s.pt')  # "Balances speed and accuracy" for object-detection
         # self.rtdetr_model = RTDETR("rtdetr-l.pt") # for OD
@@ -41,6 +48,7 @@ class YoloVitMaterialDetector:
         self.vit_processor = processor
         self.image_sub = rospy.Subscriber('/camera/image_raw', Image, self.image_callback) # TODO check which subscriber is camera for '/camera/image_raw'
         self.result_pub = rospy.Publisher('vit_inference/result', MaterialDetected, queue_size=10)  #TODO subscribe to this
+        self.result_image = rospy.Publisher('vit/inference/result', Image, queue_size=10)
         print("Initialized Detector")
 
     def image_callback(self, msg):
@@ -53,7 +61,7 @@ class YoloVitMaterialDetector:
             return
 
         # YOLO detection
-        print("YOLO detection starting")
+        # print("YOLO detection starting")
 
         results = self.yolo_model(cv_image) 
         
@@ -64,14 +72,14 @@ class YoloVitMaterialDetector:
                 crop = cv_image[y1:y2, x1:x2]
                 
                 # Vision Transformer inference
-                print("ViT inference starting")
+                # print("ViT inference starting")
                 inputs = self.vit_processor(images=crop, return_tensors="pt")
                 outputs = self.vit_model(**inputs)
                 predicted_class = outputs.logits.argmax(-1).item()
                 confidence = outputs.logits.softmax(-1).max().item()
 
                 # Saving values relevant about material detected and class
-                print("Saving material detected results of a box")
+                # print("Saving material detected results of a box")
                 det_msg = MaterialDetected()
                 det_msg.header = msg.header
                 det_msg.object_class = result.names[int(result.boxes.cls[0])]
@@ -80,24 +88,33 @@ class YoloVitMaterialDetector:
                 det_msg.y = y1
                 det_msg.width = x2-x1
                 det_msg.height = y2-y1
-                det_msg.material = str(predicted_class)
+                det_msg.material = self.labels_materials[predicted_class]
+
+                self.result_pub.publish(det_msg)
 
                 # Draw bounding box and label
-                print("DRawing bounding box and label")
+                # print("Drawing bounding box and label")
                 cv2.rectangle(cv_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(cv_image, f"Class: {predicted_class}", (x1, y1-10),
+                cv2.putText(cv_image, f"Class: {self.labels_materials[predicted_class]}", (x1, y1-10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-        # Publish material detected 
-        # try:   
-        #     self.result_pub.publish(self.bridge.cv2_to_imgmsg(result, "8UC3"))
-        # except CvBridgeError as e:
-        #     rospy.logerr(e)
+        if len(results) > 0 and len(results[0].boxes) > 0:
+            cv2.imshow("Material Detection", cv_image)
+            cv2.waitKey(1)
+            # Publish the image with detections
+            img_msg = self.bridge.cv2_to_imgmsg(cv_image, "8UC3")
+            self.result_image.publish(img_msg)
 
-                cv2.imshow("Material Detection", cv_image)
-                cv2.waitKey(1)
-                print("Material detected: "+ str(predicted_class))
-                print("COnfidence: "+ str(confidence))
+            # print("Material detected: "+ str(predicted_class) + " "+ labels_materials[predicted_class] + " "+ det_msg.object_class)
+            print(f"Material detected: {predicted_class} {self.labels_materials[predicted_class]} {det_msg.object_class}")
+            print("Confidence of material detection: "+ str(confidence))
+
+        else:
+            print("No objects detected")
+            # predicted_class = -1
+            # det_msg.material = "unknown"
+
+                
 
 if __name__ == '__main__':
     rospy.init_node('yolo_vit_mat_detector', anonymous=True)
